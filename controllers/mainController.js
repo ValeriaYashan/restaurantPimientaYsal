6be1bb8e153f765820a08c29d22455bac87b8e94
Valeria Yashan <valeriayashan@gmail.com>
2021-10-08 08:59:02 -0300

    first commit

---
+++ controllers/mainController.js
@@ -0,0 +1,42 @@
+let platos = [
+    {
+        id: 1,
+        name: 'Carpaccio fresco',
+        description: 'Entrada Carpaccio de salmón con cítricos U$S15.50'
+    },
+    {
+        id: 2,
+        name: 'Risotto de berenjena',
+        description: 'Risotto de berenjena y queso de cabra U$S 10.00'
+    },
+    {
+        id: 3,
+        name: 'Mousse de arroz',
+        description: 'Mousse de arroz con leche y aroma de azahar U$S 27.50'
+    },
+    {
+        id: 4,
+        name: 'Espárragos blancos',
+        description: 'Espárragos blancos con vinagreta de verduras y jamón ibérico U$S 37.50'
+    },
+    {
+        id: 5,
+        name: 'Empanadas',
+        description: ''
+    },
+]
+
+
+let controller = {
+    home: (req, res) => {
+        res.locals.user = 'Uriel';
+        res.render('index', {titleMenu:'Nuestro menú', platos});
+    },
+    detail: (req, res) => {
+        let plato = platos.find(plato => plato.id == req.params.id);
+        
+        res.render('detail', {plato});
+    }
+}
+
+module.exports = controller; 
---
+++ pasos que faltan.md
@@ -0,0 +1,2 @@
+Cuando el plato no posee una descripción detallada, se debe mostrar la leyenda "plato típico".
+Modificar la sección about para que el contenido se cargue directamente.
